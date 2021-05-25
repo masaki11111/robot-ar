2c9b10f7d5dfa7b91b24a7aac9b00a12a43aa130
masaki11111 <masaki.koshi1111@gmail.com>
2021-05-25 15:48:50 +0900

    seven

---
+++ Assets/Scripts/Character/Pepper/PlayerHit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHit : MonoBehaviour
+{
+    Timer timer;
+    public bool HitPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HitPlayer = false;
+        //timer = GameObject.Find("Timer").GetComponent<Timer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //enemyに当たった時
+        if (other.CompareTag("Enemy"))
+        {
+            HitPlayer = true;
+        }
+    }
+}
---
+++ Assets/Scripts/TrashCan/name.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class name : MonoBehaviour
+{
+    public GameObject box;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {//メタルスライムならタイムを十秒増やす
+        if (box.name == "box")
+        {
+            Debug.Log("1");
+        }
+    }
+}
