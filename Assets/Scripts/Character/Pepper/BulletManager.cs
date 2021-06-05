bd49249015fcdfe37aac39999cd64fa4750a1e8b
masaki11111 <masaki.koshi1111@gmail.com>
2021-06-05 15:55:15 +0900

    hoge

---
+++ Assets/Scripts/Character/Pepper/BulletManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine
+//名前空間が以下だった
+using Coffee.UIEffects;
+using System.Threading;
+//using System.Threading.Task;
+using UnityEngine.Rendering;//必要
+//using UnityEngine.Rendering.Universal//必要;
+using UnityEngine.Rendering.PostProcessing;
+
+public class BulletManager : MonoBehaviour
+{
+    // bullet prefab
+    public GameObject bullet;
+    // 弾丸発射点
+    public Transform muzzle;
+    //炎
+    public ButtonState buttonA;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // z キーが押された時
+        //if (Input.GetKeyDown(KeyCode.Z))
+
+        if (buttonA.IsDown())
+        {
+            //Debug.Log("ZZZ");
+            // 弾丸の複製
+            //GameObject bullets = Instantiate(bullet) as GameObject;
+            Instantiate(bullet, muzzle.position, muzzle.rotation, muzzle);
+
+            //Vector3 force;
+
+            //force = this.gameObject.transform.forward * speed;
+
+            // Rigidbodyに力を加えて発射
+            //bullets.GetComponent<Rigidbody>().AddForce(force);
+
+            // 弾丸の位置を調整
+            //bullets.transform.position = muzzle.position;
+        }
+    }
+    //public void BulletDestroy
+    //{
+    //    Destroy(this.gameObject);
+
+    //}
+}
---
+++ Assets/Scripts/TrashCan/BulletMover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMover : MonoBehaviour
+{
+    public float speed = 0.1f;
+    
+    void Update()
+    {
+        //transform.position += new Vector3(0, 0, 1) * Time.deltaTime * 10;
+        //transform.position += this.transform.forward * speed;
+        this.transform.position += this.transform.forward * speed;
+
+        //Debug.Log(this.transform.forward);
+    }
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    //enemyに当たった時
+    //    //if (collision.gameObject.layer == 10)
+    //    if (other.CompareTag("Enemy") || other.CompareTag("RecoverTimeEnemy"))
+    //    {
+            
+    //        //Debug.Log("hit");
+
+    //        //        //GameObject.Find("EnemyManager").GetComponent<EnemyCreation>().onDamage(damage);
+    //        //        //GameObject.Find("EnemyManager").GetComponent<EnemyCreation>().CreateEnemy;
+    //        //        //Destroy(other.gameObject);
+
+    //        //        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    //        //    }
+    //    }
+    //}
+}
